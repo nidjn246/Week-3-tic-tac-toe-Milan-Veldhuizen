0de74028679f6b313b2f4aecd2ea991a6170f3fe
nidjn246 <milanveldhuizen09@gmail.com>
2024-09-17 12:05:04 +0200

    helemaal opnieuw begonnen
    heb nu een raster, werkende turn, en je kan op elk vakje iets plaatsen

--- tic tac toe/sketch.js
+++ tic tac toe/sketch.js
@@ -1,105 +1,241 @@
 let playerturn = 1
-let lefttop1 = 0
-let lefttop2 = 0
-let righttop1 = 0
-let righttop2 = 0
-let middletop1 = 0
-let middletop2 = 0
-let leftmiddle1 = 0
-let leftmiddle2 = 0
-let rightmiddle1 = 0
-let rightmiddle2 = 0
-let middle1 = 0
-let middle2 = 0
-function setup() {
-  createCanvas(400, 500);
+let cell1 = 0
+let cell2 = 0
+let cell3 = 0
+let cell4 = 0
+let cell5 = 0
+let cell6 = 0
+let cell7 = 0
+let cell8 = 0
+let cell9 = 0
 
+function setup() 
+{
+  createCanvas(400, 500);
 }
 
-function draw() {
-  background(220);
-  //het raster
+function draw() 
+{
+  background(70, 200, 50);
+  fill (0)
+  strokeWeight (0)
+  textSize (30)
+  stroke (0)
   text ("turn: player " + playerturn, 100, 480)
   strokeWeight (3)
   line (125, 0, 125, 400)
   line (275, 0, 275, 400)
   line (0, 125, 400, 125)
   line (0, 275, 400, 275)
-  //if lefttop1 is 1 make a square at 10, 10, 10
-  if (lefttop1 == 1)
-    square (10, 10 ,10)
-  //if lefttop2 is 1 make a circle at 10, 10, 10
-  if (lefttop2 == 1)
-    circle ( 10, 10, 10)
 
-  if (righttop1 == 1)
-    square (350, 10 ,10)
-  if (righttop2 == 1)
-    circle ( 350, 10, 10)
-
-  if (middletop1 == 1)
-    square (200, 10 ,10)
-
-  if (middletop2 == 1)
-    circle ( 200, 10, 10)
-
-  if (leftmiddle1 == 1)
-    square (10, 200, 10)
-
-  if (leftmiddle2 == 1)
-    circle (10, 200, 10)
+  strokeWeight (5)
   
+
+  if (cell1 == 1)
+  {
+    fill (255)
+    strokeWeight (0)
+    square (10, 10, 10)
+  }
+  
+  if (cell1 == 2)
+  {
+    stroke ("red")
+    strokeWeight (7)
+    fill (70, 200, 50)
+    circle (60, 60, 70)
+  }
+
+  if (cell2 == 1)
+  {
+    fill (255)
+    strokeWeight (0)
+    square (150, 10, 10)
+  }
+    
+  if (cell2 == 2)
+  {
+    stroke ("red")
+    fill (70, 200, 50)
+    strokeWeight (7)
+    circle (200, 60, 70)
+  }
+
+  if (cell3 == 1)
+  {
+    fill (255)
+    strokeWeight (0)
+    square (300, 10, 10)
+  }
+      
+  if (cell3 == 2)
+  {
+    stroke ("red")
+    fill (70, 200, 50)
+    strokeWeight (7)
+    circle (340, 60, 70)
+  }
+
+  if (cell4 == 1)
+  {
+    fill (255)
+    strokeWeight (0)
+    square (10, 150, 10)
+  }
+
+  if (cell4 == 2)
+  {
+    stroke ("red")
+    fill (70, 200, 50)
+    strokeWeight (7)
+    circle (60, 200, 70)
+  }
+  
+  if (cell5 == 1)
+  {
+    fill (255)
+    strokeWeight (0)
+    square (150, 150, 10)
+  }
+  
+  if (cell5 == 2)
+  {
+    stroke ("red")
+    fill (70, 200, 50)
+    strokeWeight (7)
+    circle (200, 200, 70)
+  }
+
+  if (cell6 == 1)
+  {
+    fill (255)
+    strokeWeight (0)
+    square (350, 150, 10)
+  }
+  
+  if (cell6 == 2)
+  {
+    stroke ("red")
+    fill (70, 200, 50)
+    strokeWeight (7)
+    circle (340, 200, 70)
+  }
+
+  if (cell7 == 1)
+  {
+    fill (255)
+    strokeWeight (0)
+    square (10, 350, 10)
+  }
+  
+  if (cell7 == 2)
+  {
+    stroke ("red")
+    fill (70, 200, 50)
+    strokeWeight (7)
+    circle (60, 345, 70)
+  }
+    
+  if (cell8 == 1)
+  {
+    fill (255)
+    strokeWeight (0)
+    square (150, 350, 10)
+  }
+    
+  if (cell8 == 2)
+  {
+    stroke ("red")
+    fill (70, 200, 50)
+    strokeWeight (7)
+    circle (200, 345, 70)
+  }
+  
+  if (cell9 == 1)
+  {
+    fill (255)
+    strokeWeight (0)
+    square (350, 350, 10)
+  }
+    
+  if (cell9 == 2)
+  {
+    stroke ("red")
+    fill (70, 200, 50)
+    strokeWeight (7)
+    circle (340, 345, 70)
+  }
 }
 
 function mouseClicked ()
 {
-  if (mouseClicked && mouseX > 0 && mouseX < 400 && mouseY > 0 && mouseY < 400)
+  //cell 1
+  console.log (cell2)
+  if (mouseX > 0 && mouseX < 123 && mouseY > 0 && mouseY < 123 && cell1 == 0)
   {
-    playerturn = playerturn + 1
+    cell1 = playerturn
+    changePlayer();
+  }
+  //cell 2
+  if (mouseX > 125 && mouseX < 275 && mouseY > 0 && mouseY < 123 && cell2 == 0)
+  {
+    cell2 = playerturn
+    changePlayer();
+  }
+    
+  //cell 3
+  if (mouseX > 275 && mouseX < 400 && mouseY > 0 && mouseY < 123 && cell3 == 0)
+  {
+    cell3 = playerturn
+    changePlayer();
   }
 
-  if (playerturn == 3)
+  //cell 4
+  if (mouseX > 0 && mouseX < 123 && mouseY > 123 && mouseY < 275 && cell4 == 0)
+  {
+    cell4 = playerturn
+    changePlayer();
+  }
+
+  //cell 5
+  if (mouseX > 123 && mouseX < 275 && mouseY > 123 && mouseY < 275 && cell5 == 0)
+  {
+    cell5 = playerturn
+    changePlayer();
+  }
+  //cell 6
+  if (mouseX > 275 && mouseX < 400 && mouseY > 123 && mouseY < 275 && cell6 == 0)
+  {
+    cell6 = playerturn
+    changePlayer();
+  }
+  //cell 7
+  if (mouseX > 0 && mouseX < 123 && mouseY > 275 && mouseY < 400 && cell7 == 0)
+  {
+    cell7 = playerturn
+    changePlayer();
+  }
+  
+  //cell 8
+  if (mouseX > 123 && mouseX < 275 && mouseY > 275 && mouseY < 400 && cell8 == 0)
+  {
+    cell8 = playerturn
+    changePlayer();
+  }
+
+  //cell 9
+  if (mouseX > 275 && mouseX < 400 && mouseY > 275 && mouseY < 400 && cell9 == 0)
+  {
+    cell9 = playerturn
+    changePlayer();
+  }
+}
+
+function changePlayer()
+{
+  playerturn = playerturn + 1
+  if (playerturn > 2)
   {
     playerturn = 1
-  }
-//console.log (mouseX) 
-  if (playerturn == 2 && mouseX > 0 && mouseX < 123 && mouseY > 0 && mouseY < 123 && lefttop2 == 0)
-  {
-    lefttop1 = 1
-  }
-  
-  if (playerturn == 1 && mouseX > 0 && mouseX < 123 && mouseY > 0 && mouseY < 123 && lefttop1 == 0)
-  { 
-    lefttop2 = 1
-  }
-
-  if (playerturn == 2 && mouseX > 275 && mouseX < 400 && mouseY > 0 && mouseY < 123 && righttop2 == 0)
-  {
-    righttop1 = 1
-  }
-  
-  if (playerturn == 1 && mouseX > 275 && mouseX < 400 && mouseY > 0 && mouseY < 123 && righttop1 == 0)
-  { 
-      righttop2 = 1
-  }
-
-  if (playerturn == 2 && mouseX > 125 && mouseX < 275 && mouseY > 0 && mouseY < 123 && middletop2 == 0)
-  {
-    middletop1 = 1
-  }
-
-  if (playerturn == 1 && mouseX > 125 && mouseX < 275 && mouseY > 0 && mouseY < 123 && middletop1 == 0)
-  { 
-    middletop2 = 1
-  }
-
-  if (playerturn == 2 && mouseX > 0 && mouseX < 123 && mouseY > 125 && mouseY < 275 && leftmiddle2 == 0)
-  {
-    leftmiddle1 = 1
-  }
-
-  if (playerturn == 1 && mouseX > 0 && mouseX < 123 && mouseY > 125 && mouseY < 275 && leftmiddle1 == 0)
-  { 
-    leftmiddle2 = 1
   }
 }
